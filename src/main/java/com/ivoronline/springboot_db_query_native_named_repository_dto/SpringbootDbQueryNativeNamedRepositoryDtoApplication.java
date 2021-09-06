d72053afafbf7c4ffe903870e7030448cf45e9b3
igoreta <ivoronline@gmail.com>
2021-09-06 16:28:23 +0200

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_db_query_native_named_repository_dto/SpringbootDbQueryNativeNamedRepositoryDtoApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_db_query_native_named_repository_dto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDbQueryNativeNamedRepositoryDtoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootDbQueryNativeNamedRepositoryDtoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_db_query_native_named_repository_dto/SpringbootDbQueryNativeNamedRepositoryDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_db_query_native_named_repository_dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDbQueryNativeNamedRepositoryDtoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
